d8046858183e4f6991868c6f2e4910a35f19c0a3
tae100k <tkim3@oswego.edu>
2021-12-12 02:21:17 +0900

    props 작업 완료

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,13 +5,10 @@ import React, {useState} from 'react'; //리액트 내의 내장함수를 쓰려
 function App(){
  
   let [글제목,글제목변경] = useState(['끝말잇기 게임', '계산기', '숫자 야구 게임']);
-  글제목.map((a)=>{
-    let[따봉a, 따봉a변경] = useState(0);
-  });
-
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(false);
   let [btnTest, btnTestFunc] = useState(false);
+  let [누른제목인덱스, 누른제목인덱스변경] = useState(0);
 
    function 제목바꾸기() {
     let newArray = [...글제목];
@@ -27,31 +24,34 @@ function App(){
       <div className="black-nav">
         <div>바닐라 JS 프로젝트</div>
       </div>
-      {글제목.map((a)=> {
-    return (
-    <div className="list">
-    <h3>{a} <span onClick={()=>{
-      따봉변경(따봉a +1);
-    }}>👍</span>{따봉}</h3><p> 2월 17일 발행</p>
-    <button onClick={제목바꾸기}>수정하기</button>
-    <hr/>
-  </div>)
-   })}
+      {글제목.map((a, i)=> {
+        console.log(`i:${i} a:${a}`)
+        return (
+          <div className="list">
+            <h3 onClick={()=>{누른제목인덱스변경(i)}}>{a} <span onClick={()=>{따봉변경(따봉 +1);}}>👍</span>
+            {따봉}</h3>
+            <p> 2월 17일 발행</p>
+            <hr/>
+        </div>)
+      })
+    }
+
       
 
-
-
-      <button onClick={() => { btnTest ? btnTestFunc(false) : btnTestFunc(true)}}>버튼</button>
-      { btnTest ? <Modal/>: null}
+  <button onClick={() => { btnTest ? btnTestFunc(false) : btnTestFunc(true)}}>버튼</button>
+    { btnTest ? <Modal 글제목={글제목} 누른제목인덱스={누른제목인덱스}/>: null}
       
     </div>
   )
 }
 
-function Modal() {
+function Modal(props) {
+  console.log(props)
+  console.log(props.누른제목인덱스)
   return (
     <div className='modal'>
-      <h3>제목</h3>
+      
+      <h2>{props.글제목[props.누른제목인덱스]}</h2>
       <p>내용</p>
       <p>날짜</p>
     </div>
