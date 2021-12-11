43e1000ce47d6d2c0ca180a3d798a5be27e91e47
tae100k <tkim3@oswego.edu>
2021-12-11 21:03:22 +0900

    리액트 props 세팅

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,37 +5,44 @@ import React, {useState} from 'react'; //리액트 내의 내장함수를 쓰려
 function App(){
  
   let [글제목,글제목변경] = useState(['끝말잇기 게임', '계산기', '숫자 야구 게임']);
+  글제목.map((a)=>{
+    let[따봉a, 따봉a변경] = useState(0);
+  });
+
   let [따봉, 따봉변경] = useState(0);
+  let [modal, modal변경] = useState(false);
+  let [btnTest, btnTestFunc] = useState(false);
 
    function 제목바꾸기() {
     let newArray = [...글제목];
     newArray[0] = '틱택톡 추천';
     글제목변경(newArray); 
    };
+   
+   
+
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>바닐라 JS 프로젝트</div>
       </div>
-      <div className="list">
-        <h3>{글제목[0]} <span onClick={()=>{
-          따봉변경(따봉 +1);
-        }}>👍</span>{따봉}</h3><p> 2월 17일 발행</p>
-        <button onClick={제목바꾸기}>수정하기</button>
-        <hr/>
-        </div>
-      <div className="list">
-        <h3>{글제목[1]}</h3>
-        <p> 2월 17일 발행</p>
-        <hr/>
-      </div>
-      <div className="list">
-        <h3>{글제목[2]}</h3>
-        <p> 2월 17일 발행</p>
-        <hr/>
-      </div>
-      <Modal/>
+      {글제목.map((a)=> {
+    return (
+    <div className="list">
+    <h3>{a} <span onClick={()=>{
+      따봉변경(따봉a +1);
+    }}>👍</span>{따봉}</h3><p> 2월 17일 발행</p>
+    <button onClick={제목바꾸기}>수정하기</button>
+    <hr/>
+  </div>)
+   })}
+      
+
+
+
+      <button onClick={() => { btnTest ? btnTestFunc(false) : btnTestFunc(true)}}>버튼</button>
+      { btnTest ? <Modal/>: null}
       
     </div>
   )
@@ -43,13 +50,11 @@ function App(){
 
 function Modal() {
   return (
-    <div className="modal">
-        <h3>글제목</h3>
-        <p> 날짜</p>
-        <p> 상세내용</p>
-        <hr/>
+    <div className='modal'>
+      <h3>제목</h3>
+      <p>내용</p>
+      <p>날짜</p>
     </div>
-
   )
 }
 
